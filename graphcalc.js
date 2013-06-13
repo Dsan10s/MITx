ffe8da4ee8e4cc57d322666db14c47dbfefbaa7e
OpenShift guest <danny.ne1410s@gmail.com>
2013-06-12 23:51:16 -0400

    graphing calc works for simple functions

--- graphcalc.js
+++ graphcalc.js
@@ -7,13 +7,13 @@ var graphcalc = (function(){
 
         var div1 = $('<div id = "fxDiv"></div>');
         var div2 = $('<div id = "minMaxDiv"></div>');
-        var fxInput = $('<input id = "fxInput"></input>');
-        var minXInput = $('<input id = "minXInput"></input>');
-        var maxXInput = $('<input id = "maxXInput"></input>');
+        var fxInput = $('<input id = "fxInput" value="x*x"></input>');
+        var minXInput = $('<input id = "minXInput" value="-5"></input>');
+        var maxXInput = $('<input id = "maxXInput" value="5"></input>');
         var plotButton = $('<button id = "plotButton">Plot</button>');
         
         $(div1).append("f(x): ", fxInput);
-        $(div2).append("min X: ", minXInput,"max X: ", maxXInput);
+        $(div2).append("Min X: ", minXInput,"Max X: ", maxXInput);
         $(div).append(canvas, div1, div2, plotButton);
         
         plotButton.bind("click", function(){
@@ -21,20 +21,78 @@ var graphcalc = (function(){
         });
     }
     exports.setupGraph = setupGraph;
-    function graph(canvas, fx, minX, maxX){
+    
+    /*canvas: jquery object representing canvas element
+      fx: string representing expression to be graphed
+      minX: string representing lower bound of graph
+      mixX: string representing upper bound of graph*/
+      
+    function graph(canvas, fx, minXString, maxXString){
+        
             var ctx, tree; 
             ctx = canvas[0].getContext('2d');
-        try{
-            tree = calculator.parse(fx); //calc is defined; ignore error
-        }catch(err){
+        
+            var fxParse = calculator.parse(fx); //calc is defined; ignore error
+            var minX = parseInt(minXString, 10);
+            var maxX = parseInt(maxXString, 10);
+        /*catch(err){
             ctx.baseLineText = "middle";
             ctx.textAlign = "center";
-            ctx.font = "25px Arial";
+            ctx.font = "20px Arial";
             ctx.fillStyle = "red";
             ctx.fillText(err, 200, 150);
+        }*/
+        
+        var coordinateList = [];
+        var pixelCoordinateList = [];
+        
+        /*for loop: Loops through the x coordinates, and
+        appends to coordinateList a list of coordinates, 
+        where coordinates is as long as canvas.width();*/
+        
+        for (var x = minX; x <= maxX; x+=(-minX+maxX)/canvas.width()){
+            var y = calculator.evaluate(fxParse, {x: x});
+            var coordinates = [x, y];
+            coordinateList.push(coordinates);
         }
         
+        console.log("coordinate list created");
+        console.log("coordinate list: " + coordinateList);
         
+        /*Next obstacle: find a way to get the min element from an array*/
+        var minYTerm = coordinateList[0][1];
+        var maxYTerm = coordinateList[0][0];
+        // for loop: var minYTerm now is the min y value
+        for(var ind = 0; ind < coordinateList.length; ind++){
+            console.log("min/max Y term loop running...")
+            var newTerm = coordinateList[ind][1];
+            minYTerm = Math.min(newTerm, minYTerm);
+            maxYTerm = Math.max(newTerm, maxYTerm);
+        }
+        
+        //minY and maxY are now defined
+        console.log("minY and maxY are now defined | minY: " + minYTerm + " maxY: " + maxYTerm);
+        
+        /*for loop: maps (x, y) coordinates from coordinateList to pixel coordinates
+          then, appends these pixel coordinates to an array pixelCoordinateList*/
+        for (var px = 0; px < coordinateList.length; px++){
+            
+            var c = coordinateList[px];
+            var pixX = canvas.width()*((c[0]-minX)/(maxX-minX));
+            var pixY = canvas.height()*((maxYTerm - c[1])/(maxYTerm - minYTerm));
+            var pixelCoordinates = [pixX, pixY];
+            pixelCoordinateList.push(pixelCoordinates);
+        }
+        
+        // Now we draw to the canvas for every value in pixelCoordinateList
+        ctx.beginPath();
+        for (var i = 0; i < pixelCoordinateList.length-1; i++){
+            
+            ctx.moveTo(pixelCoordinateList[i][0], pixelCoordinateList[i][1]);
+            ctx.lineTo(pixelCoordinateList[i+1][0], pixelCoordinateList[i+1][1]);
+            
+        }
+        ctx.stroke();
     }
     return exports;
 }());
