7844b97d61a5b6f39297c500edf850a741b0871b
OpenShift guest <danny.ne1410s@gmail.com>
2013-06-14 09:08:10 -0400

    web inspector works

--- web-inspector.js
+++ web-inspector.js
@@ -10,6 +10,8 @@ var Inspector = function($) {
   // The root element of the inspector.
   var root = null;
   var inspectOn = false;
+  var tag = null;
+  var link = null;
   var template = ""
     + "<div class='tray'>"
     + "  <textarea class='text-editor'></textarea>"
@@ -17,20 +19,36 @@ var Inspector = function($) {
     + "    <div class='node-lookup'>"
     + "      <input class='selector' /><input class='nth' />"
     + "      <button class='search-button'>Search</button>"
-    + "      <button class='inspect-button'>Inspect element"
+    + "      <button class='inspect-button'>Inspect element</button>"
+    + "      <button class='toggle_handle'>Hide handle</button>"
+    + "      <span>You can also press Ctrl + Shift + e to bring down the inspector</span>"
     + "    </div>"
     + "    <div class='property-list'>"
     + "    </div>" 
+    + "    <div><p class='advice'>You can also toggle the inspector with Ctrl + Shift + e</p></div>"
     + "  </div>" 
     + "</div>" 
     + "<div class='handle'></div>";
   var toggle = function() {
       if (root.css("top") === "0px") {
         root.animate({ "top": "-300px" }, 500);
+        //$('body').animate({"top": "15px"}), 500;
+        $('*').removeClass("highlighted");
       } else {
       root.animate({ "top": "0px" }, 500);
+      //$('body').animate({"top": "315px"}, 500);
+
     };
   };
+  var toggleHandle = function(){
+    if($('.inspector .handle').is(":visible")){
+      $('.inspector .handle').hide();
+      $('.node-lookup .toggle_handle').html("Show Handle");
+    }else{
+      $('.inspector .handle').show();
+      $('.node-lookup .toggle_handle').html("Hide Handle");
+    }
+  }
   var searchBySelector = function() {
     var selectorBox = root.find(".selector");
     var selectorStr = selectorBox.val();
@@ -38,16 +56,32 @@ var Inspector = function($) {
     var html = selection.html();
     var textEditor = root.find(".text-editor");
     textEditor.val(html);
+    $('html').addClass("highlighted");
   };
+  var clickInspect = function(evt){
+    var target = $(evt.target);
+
+    textEditor = root.find(".text-editor");
+    textEditor.val(target.clone().wrap('<p>').parent().html());
+    evt.stopPropagation();
+  }
 
   var toggleInspect = function() {
       console.log("toggleInspect called");
       if (inspectOn) {
         inspectOn = false;
+        console.log('enter');
+        $('*').removeClass("highlighted");
         $('*').off("mouseenter", mouseEntered).off("mouseleave", mouseExited);
+        $(".node-lookup .inspect-button").html("Inspect element");
+
       } else {
         inspectOn = true;
+        console.log("exit");
         $("*").on("mouseenter", mouseEntered).on("mouseleave", mouseExited);
+        $("*").on("click", clickInspect);
+        $(".node-lookup .inspect-button").html("Inspect element on");
+
       };
   };
 
@@ -56,6 +90,19 @@ var Inspector = function($) {
       $(".highlighted").removeClass("highlighted");
       console.log("entered", target);
       target.addClass("highlighted");
+      tag = '<' + $(".highlighted").get(0).tagName.toLowerCase() + '>';
+
+      if (tag == '<a>'){
+        console.log("tag: " + tag);
+        link = $('.highlighted').href;
+        console.log("link: " + link);
+        $('.highlighted').href="javascript: void(0)"
+        console.log("new link: " + $('.highlighted').href);
+      }
+      
+      
+      
+
       evt.stopPropagation();
 
   };
@@ -64,12 +111,11 @@ var Inspector = function($) {
       var target = $(evt.target);
       console.log("exited", target);
       $(".highlighted").removeClass("highlighted");
+      $(".highlighted").contents().wrap(tag);
+      $('.highlighted').href = link;
       
   };
-  
-  var highlight = function() {
-      
-  };
+
   /*
    * Construct the UI
    */
@@ -77,9 +123,21 @@ var Inspector = function($) {
     root = $("<div class='inspector'></div>").appendTo($('body'));
 
     root.append(template); 
+    $('body').keyup(function(event){
+      if(event.keyCode == 17 && 16 && 69){
+        toggle();
+      }
+    });
+    $('.node-lookup .selector').keydown(function(event){
+      if(event.keyCode == 13){
+        searchBySelector();
+      }
+    })
     root.find(".handle").on("click", toggle);
+    root.find(".node-lookup .toggle_handle").on("click", toggleHandle);
     root.find(".node-lookup .search-button").on("click", searchBySelector);
     root.find(".node-lookup .inspect-button").on("click", toggleInspect);
+    //root.find('.handle').hide();
   };
 
   exports.toggle = toggle
